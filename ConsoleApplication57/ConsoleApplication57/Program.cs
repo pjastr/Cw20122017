2b9585aad3d84a1083c3a7a9ec0017586f6f5092
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-20 18:54:46 +0100

    Add files via upload

---
+++ ConsoleApplication57/ConsoleApplication57/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> napisy = new Queue<string>();
+            Queue<Osoba> osoby = new Queue<Osoba>();
+            napisy.Enqueue("pierwszy napis");
+            napisy.Enqueue("drugi napis");
+            napisy.Enqueue("trzeci napis");
+            foreach(var el in napisy)
+            {
+                Console.WriteLine(el);
+            }
+            //napisy.Peek();
+            //Console.WriteLine(napisy.Count());
+            //Console.ReadKey();
+            //napisy.Contains("pooopol");
+            //var list = napisy.Reverse().ToList();
+            //napisy = new Queue<string>(list);
+            //Osoba o1 = new Osoba("Wacław");
+            //Osoba o1 = new Osoba("Czesław");
+            while (napisy.Count != 0)
+            {
+                napisy.Dequeue();
+            }
+            
+        }
+
+        class Osoba
+        {
+            private string imie;
+            public Osoba(string imie)
+            {
+                this.imie = imie;
+            }
+        }
+        
+    }
+}
---
+++ ConsoleApplication58/ConsoleApplication58/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace ConsoleApplication58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*ArrayList liczby = new ArrayList();
+            liczby.Add(1);
+            liczby.Add("napis");
+            liczby.Add(2);
+            liczby.Insert(1, 'k');
+            Console.WriteLine(liczby[0]);
+            Console.WriteLine(liczby[liczby.Count- 1]);
+            liczby.Sort();
+             */
+            ArrayList nowa = new ArrayList();
+            nowa.Add(5d);
+            nowa.Add(4d);
+            nowa.Add(5.5d);
+            nowa.Sort();
+            //foreach(var element in nowa)
+            //{
+             //   Console.WriteLine(element);
+            //}
+           
+            nowa.Contains(5d);
+            nowa.Reverse();
+            nowa.Remove(5d);
+            nowa.RemoveAt(1);
+            nowa.Clear();
+
+            List<Osoba> osoby2 = new List<Osoba>();
+            osoby2.Add(new Osoba("Jan"));
+            osoby2.Add(new Osoba("Anna"));
+            osoby2.Add(new Osoba("Zofia"));
+            osoby2.Sort();
+            osoby2.Clear();
+            Console.ReadKey();
+        }
+    }
+
+    class Osoba:IComparable<Osoba>
+    {
+        string imie;
+
+        public Osoba(string imie)
+        { this.imie = imie; }
+
+        public int CompareTo(Osoba other)
+        {
+            return other.imie.CompareTo(this.imie);
+        }
+    }
+}
