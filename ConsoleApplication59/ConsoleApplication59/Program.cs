6335019c2d2d1b1105fab0f7133573f7e9550c34
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-20 18:49:30 +0100

    Add files via upload

---
+++ ConsoleApplication59/ConsoleApplication59/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> slownik = new Dictionary<string, int>();
+            slownik.Add("pierwsza", 1);
+            slownik["druga"] = 2;
+            slownik["druga"] = 2;
+            foreach(KeyValuePair<string, int> kvp in slownik)
+            {
+                Console.WriteLine("{"+kvp.Key+";"+kvp.Value+"}");
+            }
+            Console.WriteLine(slownik.First());
+            Console.WriteLine(slownik.Count());
+            slownik.ContainsKey("pierwsza");
+            slownik.Clear();
+        }
+    }
+}
